6973d326a4e6be148b62d5900c6002d4393dc67a
David Daniel <davidbd7@gmail.com>
2023-12-06 14:42:13 -0800

    finished about and project section

---
+++ components/about.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React from 'react'
+import SectionHeading from './section-heading'
+import { motion} from 'framer-motion'
+
+export default function About() {
+    return <motion.section className ="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28 "
+                            initial = {{opacity: 0, y: 100}}
+                            animate = {{opacity: 1, y: 0}}
+                            transition = {{
+                                delay: 0.175
+                            }}
+                            id = "about">
+        <SectionHeading>About Me</SectionHeading>
+        <p className="mb-3">
+
+            I am a recent <span className = "font-bold"> Computer Science graduate </span> from <span className = "italic"> San Jose State University </span> located in the heart of the Silicon Valley. 
+            Through my <span className = "underline">education, internship experience, and personal projects</span>,  I have gained a strong foundation in <span className = "font-bold"> Full-Stack Development </span>. The Languages I am <span className = "italic"> most proficient </span>
+            in are {" "} <span className="font-medium">Python, Java, React, and Typescript. </span> I love learning new technologies, and am currently in search of a <span className="font-medium">full-time position </span> as a Software Engineer.
+        </p>
+        <p>
+        <span className="italic">When I'm not coding</span>, I enjoy working out, fashion, and playing basketball. I also enjoy{" "}
+        <span className="font-medium">picking up new hobbies</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">the art of espresso, and have thouroughly enjoyed learning all of the intricate details that go into making a cup of coffee</span>. 
+      </p>
+    </motion.section>
+}
--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
     return (
-        <section className = "mb-28 max-w-[50rem] text-center sm:mb-0"> 
+        <section id = "home" className = "mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"> 
             <div className = 'flex items-center justify-center'> 
                 <div className = "relative">
                     <motion.div 
@@ -46,7 +46,7 @@ export default function Intro() {
                 </div>
             </div>
         
-        <motion.p className = "mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        <motion.h1 className = "mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
                   initial = {{opacity: 0, y: 100}}
                   animate = {{opacity: 1, y: 0}}
                   transition = {{
@@ -59,7 +59,7 @@ export default function Intro() {
             </span> I'm a {" "} <span className = "font-bold "> full-stack developer</span> with {" "} <span className = "font-bold"> 1 year</span> of experience.
             I am a recent graduate with <span className = "font-bold">industry experience</span> looking for <span className = "italic"> new oppurtunities</span> in both 
             <span className = "underline"> Back-End and Full-Stack Roles! </span>
-        </motion.p>
+        </motion.h1>
 
         <motion.div className = "flex flex-col sm:flex-row justify-center gap-2 px-4 text-lg font-medium"
                     initial = {{opacity: 0, y: 100}}
@@ -70,15 +70,21 @@ export default function Intro() {
                     }}>
                  
             <Link href = "#contact"
-                  className = "group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"> 
+                  className = "group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 \
+                  hover:scale-110 hover:bg-gray-950 active:scale-105 transition cursor-pointer"> 
                 
                 Contact me here <BsArrowRight  className = "opacity-70 group-hover:translate-x-1"  /> 
 
             </Link>
 
-            <a className = "bg-white px-7 py-3 flex items-center gap-2 rounded-full"> Download CV <HiDownload /></a>
-            <a className = "bg-white py-4 text-gray-900 flex items-center gap-2 rounded-full"> <BsLinkedin /> </a>
-            <a className = "bg-white py-4 text-gray-900 flex items-center gap-2  text-[1.35rem] rounded-full"> <FaGithubSquare/> </a>
+            <a className = "group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 \
+            hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href = "/CV.pdf" download> Download CV <HiDownload className = "opacity-60 group-hover:translate-y-1 transition"/></a>
+
+            <a className = "bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] \
+            hover:scale-[1.15] hover: text-gray-950 active:scale-105 transition cursor-pointer border border-black/10" href = "https://linkedin.com" target = "_blank"> <BsLinkedin /> </a>
+
+            <a className = "bg-white p-4 text-gray-700 flex items-center gap-2  text-[1.35rem] rounded-full focus:scale-[1.15] \
+            hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10" href = "https://github.com" target = "_blank"> <FaGithubSquare/> </a>
 
 
 
