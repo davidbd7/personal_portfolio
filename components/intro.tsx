46532498f8b338b2f6eda3d4ffa2f137976e286f
David Daniel <davidbd7@gmail.com>
2023-12-29 09:15:22 -0800

    updated resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -91,7 +91,7 @@ export default function Intro() {
             </Link>
 
             <a className = "group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 \
-            hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href = "/dec_11_resume.pdf" download> Download CV <HiDownload className = "opacity-60 group-hover:translate-y-1 transition"/></a>
+            hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href = "/dec_29_david.pdf" download> Download CV <HiDownload className = "opacity-60 group-hover:translate-y-1 transition"/></a>
 
             <a className = "bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] \
             hover:scale-[1.15] hover: text-gray-950 active:scale-105 transition cursor-pointer border border-black/10" href = "https://www.linkedin.com/in/david-daniel-fullstack/" target = "_blank"> <BsLinkedin /> </a>
